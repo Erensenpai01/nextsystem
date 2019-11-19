08dad629e79fb516bfc1497dac54a40870434b9c
Erensenpai01 <49330405+Erensenpai01@users.noreply.github.com>
2019-11-19 15:47:24 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -57,7 +57,7 @@ const queue = new Map();
 client.on('ready', function() {
 	console.log(`i am ready ${client.user.username}`);
     client.user.setGame('𝜠𝙧𝙚𝞰 𝙎𝙚𝞰𝙥𝙖𝒊 💯',"https://www.twitch.tv/peery13");
-});🆒️
+});
 
 
 client.on('message', message => {
