c28a5306560abe275706047ee4db5596bafa797d
Erensenpai01 <49330405+Erensenpai01@users.noreply.github.com>
2019-11-19 15:50:42 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -56,7 +56,7 @@ if (message.content.startsWith(prefix + 'setgame')) {
 const queue = new Map();
 client.on('ready', function() {
 	console.log(`i am ready ${client.user.username}`);
-    client.user.setGame('𝜠𝙧𝙚𝞰 𝙎𝙚𝞰𝙥𝙖𝒊 💯',"https://www.twitch.tv/peery13");
+    client.user.setGame('𝜠𝙧𝙚𝞰 𝙎𝙚𝞰𝙥𝙖𝒊',"https://www.twitch.tv/peery13");
 });
 
 
